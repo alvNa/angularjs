3cb83bf86f4b21f0e0e4c884b26958ebe4b191cd
alvNa <alvaro.nav@gmail.com>
2016-01-02 00:46:23 +0100

    adding karma runner 4 testing

---
+++ nga-ui/gulpfile.conf.js
@@ -0,0 +1,59 @@
+var tsjsmapjsSuffix = '.{ts,js.map,js}';
+
+var bower = 'bower_components/';
+var src = 'src/';
+
+var config = {
+    src: src, 
+    base: '.',
+    buildDir: './build/',
+    debug: 'debug',
+    release: 'release',
+    css: 'css',
+    karmaConf: 'karma.conf.js',
+    baseDir: __dirname,
+
+    bootFile: src + 'index.html',
+    bootjQuery: bower + 'jquery/dist/jquery.min.js',
+
+    // The fonts we want Gulp to process
+    fonts: [bower + 'fontawesome/fonts/*.*'],
+
+    images: 'images/**/*.{gif,jpg,png}',
+
+    // The scripts we want Gulp to process
+    scripts: [
+        // Bootstrapping
+        src + 'app' + tsjsmapjsSuffix,
+        src + 'config.route' + tsjsmapjsSuffix,
+
+        // common Modules
+        src + 'common/common' + tsjsmapjsSuffix,
+        src + 'common/logger' + tsjsmapjsSuffix,
+        src + 'common/spinner' + tsjsmapjsSuffix,
+
+        // common.bootstrap Modules
+        src + 'common/bootstrap/bootstrap.dialog' + tsjsmapjsSuffix,
+
+        // directives
+        src + 'directives/**/*' + tsjsmapjsSuffix,
+
+        // services
+        src + 'services/**/*' + tsjsmapjsSuffix,
+    ],
+
+    // The styles we want Gulp to process
+    styles: [
+        'content/styles.css'
+    ]
+};
+
+config.debugFolder = config.buildDir + config.debug + '/';
+config.releaseFolder = config.buildDir + config.release + '/';
+
+config.templateFiles = [
+    src + '**/*.html',
+    '!' + config.bootFile // Exclude the launch page
+];
+
+module.exports = config;
--- nga-ui/gulpfile.js
+++ nga-ui/gulpfile.js
@@ -1,6 +1,15 @@
-var gulp = require('gulp');
+'use strict';
 
-gulp.task('default', function() {
-  // place code for your default task here
-  console.log('default');
-});
+var gulp = require('gulp');
+// Get our config
+var config = require("./gulpfile.conf.js");
+require('mod-clean')(gulp,config);
+require('mod-build')(gulp,config);
+require('mod-server')(gulp,config);
+require('mod-test')(gulp,config);
+
+gulp.task('test', ['pre-test','karma-run']);
+gulp.task('ci', ['pre-test','karma-single-run']);
+//gulp.task('serve', ['inject', 'browser-sync']);
+
+gulp.task('default', ['ci']);
